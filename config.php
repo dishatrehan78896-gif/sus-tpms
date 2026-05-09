71b28d1b3f9c290a74b96e64ccf01430fa4de181
Disha Trehan <dishatrehan78896@gmail.com>
2026-05-09 09:11:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
 
-// Database configuration
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "SUS";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if user is logged in
+
 function isLoggedIn() {
     return isset($_SESSION['user_id']);
 }
 
-// Redirect if not logged in
+
 function requireLogin() {
     if (!isLoggedIn()) {
         header("Location: login.php");
